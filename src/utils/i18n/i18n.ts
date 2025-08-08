f4a8d30b7e221adc5f0a427d4278e2e65501ce3b
BlequiOFC <126295815+BlequiOFC@users.noreply.github.com>
2025-08-08 18:23:10 -0300

    initial files and i18n ready

---
+++ src/utils/i18n/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enLang from './locales/en/en.json';
+import ptBRLang from './locales/pt-BR/pt-BR.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enLang,
+      },
+      'pt-BR': {
+        translation: ptBRLang,
+      },
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
