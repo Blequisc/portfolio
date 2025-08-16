5efe5775fac38e71e486cf5f486dd695fa270aad
Blequisc <gustavopregigueiro6@gmail.com>
2025-08-16 20:37:52 -0300

    added motion library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import i18n from "i18next"
 import Header from "./components/Header";
+import { useTranslation } from "react-i18next";
 
 function App() {
-
+  const { t } = useTranslation();
   return (
     <>
       <Header />
       {i18n.language}
+      <br/> 
+      {t("helloWorld")}
     </>
   );
 }
--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -1,11 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enLang from './locales/en/en.json';
 import ptBRLang from './locales/pt-BR/pt-BR.json';
 
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -16,7 +18,7 @@ i18n
         translation: ptBRLang,
       },
     },
-    lng: "pt-BR", // default language
+    // lng: "pt-BR", // default language
     fallbackLng: 'en', // fallback language, in case default language doesn't work
     debug: true,
     interpolation: {
