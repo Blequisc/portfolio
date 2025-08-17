7d949d2580cc932b0da2815e47c110340996d965
Blequisc <gustavopregigueiro6@gmail.com>
2025-08-16 21:26:46 -0300

    react typed added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import "./App.css";
-import i18n from "i18next"
+import i18n from "i18next";
 import Header from "./components/Header";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 
 function App() {
   const { t } = useTranslation();
   return (
     <>
-      <Header />
-      {i18n.language}
-      <br/> 
-      {t("helloWorld")}
+      <motion.div
+        key={i18n.language} // força re-render quando idioma muda
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Header />
+        {i18n.language}
+        <br />
+        {t("helloWorld")}
+      </motion.div>
     </>
   );
 }
